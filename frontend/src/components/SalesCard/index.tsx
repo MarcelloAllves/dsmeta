8ff77f5039a9b86f36945f4026c496566f0992ca
MarcelloAllves <marcello.allves@hotmail.com>
2022-09-09 21:55:30 -0300

    Date UpDate

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -20,11 +20,15 @@ function SalesCard() {
     const [sales, setSales] = useState<Sale[]>([]);
 
     useEffect(() => {
-        axios.get(`${BASE_URL}/sales`)// Busca do back end as vendas.
+
+        const dtaMin = minDate.toISOString().slice(0,10);
+        const dtaMax = maxDate.toISOString().slice(0,10);
+
+        axios.get(`${BASE_URL}/sales?minDate=${dtaMin}&maxDate=${dtaMax}`)// Busca do back end as vendas.
             .then(response => {
                 setSales(response.data.content)
             })
-    }, [])
+    }, [minDate,maxDate])
 
     return (
         <>
